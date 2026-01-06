3ce90939e2fbbc54ed43fb3a309b18451301ae41
marcosEvo12 <marcos.alves@evonetwork.com.br>
2026-01-06 16:08:59 -0300

    Exercício: Manipulação do DOM

--- Manipulação do DOM  Lógica JS/script.js
+++ Manipulação do DOM  Lógica JS/script.js
@@ -1,23 +1,51 @@
-const premios = ["maça"];
+const premios = ["controle" , "videogame" , "carrinho"];
 
 function montarItensNaTela() {
   const lista = document.querySelector(".premios ul");
 
   premios.forEach(function(item) {
-    lista.innerHTML = `<li>${item}</li>`;
+    lista.innerHTML += `<li>${item}</li>`;
   });
 }
 
 montarItensNaTela();
 
-/* essa função deve ser acionada quando o usuário clica em um prêmio */
+
+
+const itens = document.querySelectorAll(".premios ul li");
+
+itens.forEach((item) => {
+  item.addEventListener("click", function () {
+    escolherItem(this);
+  });
+});
+
 function escolherItem(elemento) {
-  elemento.classList.toggle("selecionado");
+  itens.forEach((item) =>{
+    item.classList.remove("selecionado")
+  });
+  elemento.classList.add("selecionado")
 }
 
-/* essa função deve ser chamada quando o botão de confirmar for pressionado */
+
+
 function confirmarPremio() {
-  // seu código aqui
+  const selecionado = document.querySelector(".selecionado");
+
+
+  if (!selecionado) {
+    alert("Você precisa escolher um prêmio");
+    return;
+  }
+
+  const naoEscolhidos = [];
+
+  itens.forEach((item) => {
+    if (!item.classList.contains("selecionado")) {
+      naoEscolhidos.push(item.innerText);
+    }
+  });
+
+  alert("Prêmios não escolhidos: " + naoEscolhidos.join(", "));
 }
 
-montarItensNaTela();
